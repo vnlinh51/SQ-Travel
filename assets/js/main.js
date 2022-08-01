ccac200ff7be95472de4394a71a3a5675c382b51
Vu Ngoc Linh <vnlinh51@gmail.com>
2022-08-01 20:10:55 +0700

    fix menu and subscribe button

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,7 +102,7 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    const sectionId = current.getAttribute('id');
+    sectionId = current.getAttribute('id');
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
